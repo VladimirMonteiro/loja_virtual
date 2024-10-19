ecb893bfb647db2f46334dd421a912c18e6d0a61
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-19 11:22:17 -0300

    adicionado hash code e equals

--- src/main/java/com/outercode/lojaVirtual/models/MarcaProduto.java
+++ src/main/java/com/outercode/lojaVirtual/models/MarcaProduto.java
@@ -4,11 +4,12 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.Objects;
 
 
 @Entity
 @Table(name = "marca_prouto")
-@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1)
+@SequenceGenerator(name = "seq_marca_produto", sequenceName = "seq_marca_produto", allocationSize = 1, initialValue = 1)
 public class MarcaProduto implements Serializable {
 
     @Serial
@@ -34,5 +35,17 @@ public class MarcaProduto implements Serializable {
 
     public void setNomeDesc(String nomeDesc) {
         this.nomeDesc = nomeDesc;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MarcaProduto that)) return false;
+        return Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId());
     }
 }

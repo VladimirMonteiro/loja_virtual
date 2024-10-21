b6b4975ecf53daaa41eacc761bb3d46d2f036f7e
vladimirmonteiro <vladimir.monteiro021@gmail.com>
2024-10-20 22:22:51 -0300

    aicionado tipo de endereço e seus getter e setter

--- src/main/java/com/outercode/lojaVirtual/models/Endereco.java
+++ src/main/java/com/outercode/lojaVirtual/models/Endereco.java
@@ -1,5 +1,6 @@
 package com.outercode.lojaVirtual.models;
 
+import com.outercode.lojaVirtual.models.enums.TipoEndereco;
 import jakarta.persistence.*;
 
 import java.io.Serial;
@@ -29,6 +30,18 @@ public class Endereco implements Serializable {
     @ManyToOne(targetEntity = Pessoa.class)
     @JoinColumn(name = "pessoa_id", nullable = false, foreignKey = @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
     private Pessoa pessoa;
+
+    @Enumerated(EnumType.STRING)
+    private TipoEndereco tipoEndereco;
+
+
+    public TipoEndereco getTipoEndereco() {
+        return tipoEndereco;
+    }
+
+    public void setTipoEndereco(TipoEndereco tipoEndereco) {
+        this.tipoEndereco = tipoEndereco;
+    }
 
     public Long getId() {
         return id;

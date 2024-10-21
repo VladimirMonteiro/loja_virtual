b3c3f51f8395fce5b62951b64d942f8c3edc95b2
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-10-21 20:30:43 -0300

    adicionado EntityScan

--- src/main/java/com/outercode/lojaVirtual/LojaVirtualApplication.java
+++ src/main/java/com/outercode/lojaVirtual/LojaVirtualApplication.java
@@ -2,8 +2,10 @@ package com.outercode.lojaVirtual;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.outercode.lojaVirtual.models")
 public class LojaVirtualApplication {
 
 	public static void main(String[] args) {

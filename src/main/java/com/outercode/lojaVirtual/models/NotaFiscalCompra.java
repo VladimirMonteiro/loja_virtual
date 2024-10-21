6641a44002327abe1b72c753258fb42f36ce22e7
Vladimir Monteiro Souza de Lima <vladimir.monteiro021@gmail.com>
2024-10-21 20:29:37 -0300

    classe nota fiscal de compra

---
+++ src/main/java/com/outercode/lojaVirtual/models/NotaFiscalCompra.java
@@ -0,0 +1,42 @@
+package com.outercode.lojaVirtual.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@Entity
+@Table(name = "nota_fiscal_compra")
+@SequenceGenerator(name = "seq_nota_fiscal_compra", sequenceName = "seq_nota_fiscal_compra", initialValue = 1, allocationSize = 1)
+public class NotaFiscalCompra implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_nota_fiscal_compra")
+    private Long id;
+
+    private String numeroNota;
+    private String serieNota;
+    private String descricaoObs;
+    private BigDecimal valorTotal;
+    private BigDecimal valorDesconto;
+    private BigDecimal valorIcms;
+
+    @Temporal(TemporalType.DATE)
+    private Date dataCompra;
+
+    @ManyToOne(targetEntity = Pessoa.class)
+    @JoinColumn(name = "pessoa_id", nullable = false, foreignKey =
+    @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "pessoa_fk"))
+    private Pessoa pessoa;
+
+    @ManyToOne
+    @JoinColumn(name = "conta_pagar_id", nullable = false, foreignKey =
+    @ForeignKey(value = ConstraintMode.CONSTRAINT, name = "conta_pagar_fk"))
+    private ContaPagar contaPagar;
+
+}
